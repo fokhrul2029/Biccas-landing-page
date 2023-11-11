9f39b6ceb3ee533dd8edca1612eb82b4c9d32d67
fokhrul2029 <ifokhrul.2029@gmail.com>
2023-11-11 21:11:00 +0600

    update navbar, footer and added hover effect

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Navigation Js-----------
+// When the user scrolls down 120px from the top of the document,add a box-shadow css on the navbar
+window.onscroll = () => {
+    if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
+      document.querySelector("#navbar").style.boxShadow = "0 0 27px 0px #0000001a ";
+    } else {
+      document.querySelector("#navbar").style.boxShadow = "0 0 0 transparent ";
+    }
+  };
